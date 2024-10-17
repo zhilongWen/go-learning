a4f337ba3892c0c00db2464ed49780b1d0bda8ad
wenzhilong <wenzhilong@wenzhilongdeMacBook-Pro.local>
2024-10-18 00:41:17 +0800

    tcp

---
+++ base-learning/day13-tcp/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	addr, _ := net.ResolveTCPAddr("tcp", ":8088")
+	tcp, _ := net.ListenTCP("tcp", addr)
+
+	for {
+
+		conn, err := tcp.AcceptTCP()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn *net.TCPConn) {
+	fmt.Println("New connection from", conn.RemoteAddr().String())
+
+	//buf := make([]byte, 1024)
+	//str, _ := conn.Read(buf)
+	//fmt.Println("Received data:", string(buf[:str]))
+
+	for {
+		buf := make([]byte, 1024)
+		str, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Println("Received data:", string(buf[:str]))
+
+		conn.Write([]byte("server received your message:" + string(buf[:str])))
+	}
+}
